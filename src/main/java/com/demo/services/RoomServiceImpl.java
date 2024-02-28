2e2a987382b9dddbbc5d3ab55855fdb03b20614f
kenchiwar <huynhtienthang1264@gmail.com>
2024-02-28 16:47:25 +0700

    met day

--- src/main/java/com/demo/services/RoomServiceImpl.java
+++ src/main/java/com/demo/services/RoomServiceImpl.java
@@ -7,48 +7,43 @@ import com.demo.entities.Room;
 import com.demo.repositories.RoomRepository;
 
 @Service
-public class RoomServiceImpl implements RoomService{
+public class RoomServiceImpl implements RoomService {
 	@Autowired
-    private RoomRepository repositoryRoom;
-    
-    @Override
-    public boolean delete(int id) {
-        try {
-        	repositoryRoom.delete(find(id));
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	private RoomRepository repositoryRoom;
 
-    @Override
-    public Iterable<Room> findAll() {
-        return repositoryRoom.findAll();
-    }
+	@Override
+	public boolean delete(int id) {
+		try {
+			repositoryRoom.delete(find(id));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-    @Override
-    public boolean save(Room Room) {
-        try {
-        	if (!(Room.getPrice() >= 0 && 
-        		    Room.getPriceDiscount() >= 0 && 
-        		    Room.getRoomMax() >= 0 && 
-        		    Room.getRoomNow() >= 0 && 
-        		    Room.getPeopleMin() >= 0 && 
-        		    Room.getPeopleMax() >= 0)) 
-        		
-        	System.out.println("mệt mỏi ghê") ;
-        	
-        	repositoryRoom.save(Room);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	@Override
+	public Iterable<Room> findAll() {
+		return repositoryRoom.findAll();
+	}
 
-    @Override
-    public Room find(int id) {
-        return repositoryRoom.findById(id).get();
-    }
+	@Override
+	public boolean save(Room Room) {
+		try {
+			if (!(Room.getPrice() >= 0 && Room.getPriceDiscount() >= 0 && Room.getRoomMax() >= 0
+					&& Room.getRoomNow() >= 0 && Room.getPeopleMin() >= 0 && Room.getPeopleMax() >= 0))
+				return false;
+
+			repositoryRoom.save(Room);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public Room find(int id) {
+		return repositoryRoom.findById(id).get();
+	}
 }
